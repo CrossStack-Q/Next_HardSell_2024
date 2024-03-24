33ff945423eaa1353c1dc220c463e093ea860070
CrossStack-Q <CrossStack@gmail.com>
2024-03-24 23:32:48 +0530

    doing routing

--- client/supabase.ts
+++ client/supabase.ts
@@ -3,7 +3,7 @@ import {createBrowserClient} from "@supabase/ssr"
 
 export const createClient = ()=>(
     createBrowserClient(
-        process.env.SUPABASE_URL,
-        process.env.SUPABASE_ANON_KEY 
+        process.env.SUPABASE_URL!,
+        process.env.SUPABASE_ANON_KEY!
     )
 )
